99d3cac2c1d9ca731dfe9ac0bffc39899282da73
Santiago Basulto <santiago.basulto@gmail.com>
2017-10-17 14:23:30 -0300

    Bump version

--- slack_cli/__init__.py
+++ slack_cli/__init__.py
@@ -1,7 +1,7 @@
 from .api import HighLevelSlackClient, HighLevelSlackAPIException
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __title__ = 'slack.py'
 __license__ = 'MIT'
 __description__ = 'A simple command line tool to interact with Slack'
